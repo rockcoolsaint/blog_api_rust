024aabd7e638649a7787bc0c8fd0e3c2ee0b5120
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2024-09-16 23:10:26 +0100

    Extract data from middleware

--- src/routes/handlers/user_handlers.rs
+++ src/routes/handlers/user_handlers.rs
@@ -1,10 +1,46 @@
-use actix_web::{get, web, Responder};
+use std::fmt::format;
 
-use crate::utils::{api_response, app_state};
+use actix_web::{get, post, web, Responder};
+use sea_orm::{ActiveModelTrait, EntityTrait, IntoActiveModel, Set, Update};
+use serde::{Deserialize, Serialize};
+
+use crate::utils::{api_response, app_state, jwt::Claims};
+
+#[derive(Serialize, Deserialize)]
+struct UpdateUserModel {
+  name: String
+}
 
 #[get("")]
 pub async fn user(
-  app_state: web::Data<app_state::AppState>
-) -> impl Responder {
-  api_response::ApiResponse::new(200, "Verified user".to_string())
+  app_state: web::Data<app_state::AppState>,
+  claim_data: Claims
+) -> Result<api_response::ApiResponse, api_response::ApiResponse> {
+
+  let user_model = entity::user::Entity::find_by_id(claim_data.id)
+  .one(&app_state.db).await
+  .map_err(|err| api_response::ApiResponse::new(500, err.to_string()))?
+  .ok_or(api_response::ApiResponse::new(404, "User Not Found".to_owned()))?;
+  
+  Ok(api_response::ApiResponse::new(200, format!(" {{ 'name': '{}', 'email': '{}' }} ", user_model.name, user_model.email)))
+}
+
+#[post("update")]
+pub async  fn update_user(
+  app_state: web::Data<app_state::AppState>,
+  user_data: web::Json<UpdateUserModel>,
+  claim_data: Claims
+) -> Result<api_response::ApiResponse, api_response::ApiResponse> {
+
+  let mut user_model = entity::user::Entity::find_by_id(claim_data.id)
+  .one(&app_state.db).await
+  .map_err(|err| api_response::ApiResponse::new(500, err.to_string()))?
+  .ok_or(api_response::ApiResponse::new(404, "User Not Found".to_owned()))?
+  .into_active_model();
+  
+  user_model.name = Set(user_data.name.clone());
+  user_model.update(&app_state.db).await
+  .map_err(|err| api_response::ApiResponse::new(500, err.to_string()))?;
+
+  Ok(api_response::ApiResponse::new(200, "User updated".to_owned()))
 }
