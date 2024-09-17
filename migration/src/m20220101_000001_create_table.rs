97067f2a57ea0d49ffa355e06cade3f1644815bf
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2024-09-17 23:45:56 +0100

    Post Schema migration file

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -1,13 +1,13 @@
 use sea_orm_migration::{prelude::*, schema::*};
 
+use crate::m20240903_220737_create_user_table::User;
+
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
 
         manager
             .create_table(
@@ -15,16 +15,24 @@ impl MigrationTrait for Migration {
                     .table(Post::Table)
                     .if_not_exists()
                     .col(pk_auto(Post::Id))
-                    .col(string(Post::Title))
-                    .col(string(Post::Text))
+                    .col(string(Post::Title).not_null())
+                    .col(string(Post::Text).not_null())
+                    .col(string(Post::Uuid).uuid().unique_key().not_null())
+                    .col(string(Post::Image).string())
+                    .col(string(Post::UserId).integer().not_null())
+
+                    .foreign_key(
+                        ForeignKey::create()
+                        .name("fk-posts-users-id")
+                        .from(Post::Table, Post::Uuid)
+                        .to(User::Table, User::Id)
+                    )
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
 
         manager
             .drop_table(Table::drop().table(Post::Table).to_owned())
@@ -38,4 +46,7 @@ enum Post {
     Id,
     Title,
     Text,
+    Uuid,
+    Image,
+    UserId
 }
